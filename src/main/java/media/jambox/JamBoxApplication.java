e600ac2f92fa4cdadd0f595de8c3ce6f47d900e7
Frank Pasqualini <FJP11@pitt.edu>
2020-01-22 10:02:09 -0500

    Potential react gradle implementation

---
+++ src/main/java/media/jambox/JamBoxApplication.java
@@ -0,0 +1,13 @@
+package media.jambox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JamBoxApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(JamBoxApplication.class, args);
+    }
+}
