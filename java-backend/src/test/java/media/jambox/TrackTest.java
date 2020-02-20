f8209229e4ecd6174d0c258777343a53c7eb535a
Frank Pasqualini <FJP11@pitt.edu>
2020-02-19 21:25:20 -0500

    Added methods and cleaned up imports

--- java-backend/src/test/java/media/jambox/TrackTest.java
+++ java-backend/src/test/java/media/jambox/TrackTest.java
@@ -1,30 +1,57 @@
 package media.jambox;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import com.wrapper.spotify.exceptions.SpotifyWebApiException;
+import java.io.IOException;
+import org.junit.Before;
+import org.junit.Test;
 
 public class TrackTest
 {
-    @org.junit.Test
-    public void testTrack()
-        throws java.io.IOException, com.wrapper.spotify.exceptions.SpotifyWebApiException
+    private final transient String accessToken = "BQBBsBGFKhsHbBiGYU3sohsh0RtKJbuLgRu9CuTlEOJ4JFeG_CM_MwMY_LAuM7Q4dVfi0262y3E8LYgWmNmJc0mb6XE6bWu9Dzh5d9bhTAW_aA0Uc4csiQH_TEnGjvFs4lcFYkmGjV97xSC5IbkFiVC8R4Q_Brc";
+    private transient Track testTrack;
+
+    @Before
+    public void setUp()
+        throws IOException, SpotifyWebApiException
     {
-        final String accessToken = "BQCjm_vxykeaXCWOTOTFj2q1-fm7c1JtqtiOSrxRSfk19w7FoWI77Wh0W93JD50lYRIkoV8R5F-fY5kUdWuTVgPShQg40x_GMVDQMTDf1CMRBye-wcd3GkZbbAQzPGk3cLx_vbeguZxLT8U";
         final String id = "7GhIk7Il098yCjg4BQjzvb";
+        testTrack = new Track(id, accessToken);
+    }
 
-        Track testTrack = new Track(id, accessToken);
-
-        assertEquals(id, testTrack.getId());
+    @Test
+    public void testTrackInfo()
+    {
+        assertEquals("7GhIk7Il098yCjg4BQjzvb", testTrack.getId());
         assertEquals("Never Gonna Give You Up", testTrack.getName());
         assertEquals(212826, testTrack.getDurationMS());
-        assertEquals(0, testTrack.getScore());
-        testTrack.incrementScore();
-        assertEquals(1, testTrack.getScore());
-        testTrack.decrementScore();
         assertEquals(0, testTrack.getScore());
         assertEquals("Whenever You Need Somebody", testTrack.getAlbumName());
         assertEquals(3, testTrack.getAlbumImages().length);
         assertEquals("https://i.scdn.co/image/ab67616d0000b273237665d08de01907e82a7d8a", testTrack.getAlbumImages()[0]);
         assertEquals(1, testTrack.getArtistNames().length);
         assertEquals("Rick Astley", testTrack.getArtistNames()[0]);
+    }
+
+    @Test
+    public void testCompare()
+        throws java.io.IOException, com.wrapper.spotify.exceptions.SpotifyWebApiException
+    {
+        final String id2 = "6rPO02ozF3bM7NnOV4h6s2";
+
+        Track testTrack2 = new Track(id2, accessToken);
+
+        testTrack.incrementScore();
+        testTrack2.decrementScore();
+
+        assertTrue(testTrack.compareTo(testTrack2) > 0);
+    }
+
+    @Test
+    public void testString()
+    {
+        assertEquals("Never Gonna Give You Up - [Rick Astley]", testTrack.toString());
     }
 }
