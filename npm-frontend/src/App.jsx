1e9e50b417c6033a06222d9f958d7e1607c8658a
Ashley Stauffer <ashleystauffer@Ashleys-MacBook-Pro.local>
2020-02-21 17:12:52 -0500

    Added logo in top left corner

--- npm-frontend/src/App.jsx
+++ npm-frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import Logo from './Logo'
 import SpotifyLogo from './spotifylogo.png';
 
 const CreateEventButton = styled.button`
@@ -14,12 +15,13 @@ const CreateEventButton = styled.button`
   background-color: #EBEBEB;
   border-color: #272727;
   box-shadow: inset 0px 0px 21px 0px #272727, 13px 12px 20px -5px black;
-  transition: width .5s, height .5s, font-size .5s;
+  transition: width .5s, height .5s, font-size .5s, background-color .5s;
 
   &:hover {
     height: 100px;
     width: 215px;
     font-size: 28px;
+    background-color: #EBEBF1;
   }
 
 `;
@@ -36,25 +38,14 @@ const JoinEventButton = styled.button`
   border-color: #272727;
   font-weight: bold;
   box-shadow: inset 0px 0px 30px #000000, 13px 12px 20px -5px black;
-  transition: width .5s, height .5s, font-size .5s;
+  transition: width .5s, height .5s, font-size .5s, background-color .5s;
 
   &:hover {
     height: 170px;
     width: 380px;
     font-size: 55px;
+    background-color: #2A77D6;
   }
-`;
-
-const LogoJ = styled.div`
-  color: #2A77C9;
-  font-size: 250px;
-  margin: -35px;
-`;
-
-const LogoTitle = styled.div`
-  color: #EBEBEB;
-  font-size: 50px;
-  font-weight: bold;
 `;
 
 const HomePageContainer = styled.div`
@@ -64,15 +55,9 @@ const HomePageContainer = styled.div`
   padding: 30px;
 `;
 
-const LogoContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
 const Background = styled.div`
   background-color: #272727;
-  height: 90vh;
+  min-height: 100vh;
   width: 100%;
 `;
 
@@ -86,23 +71,21 @@ const Spotify = styled.img`
 `;
 
 const TopBar = styled.div`
-  height: 65px;
+  height: 60px;
   background-color: #EBEBEB;
+  display: flex;
 `;
 
 class App extends Component {
   render() {
     return (
       <div>
-        <TopBar />
+        <TopBar><Logo isLogo={true}/></TopBar>
         <Background>
           <HomePageContainer>
-            <LogoContainer>
-              <LogoJ>J</LogoJ> 
-              <LogoTitle>JAMBOX</LogoTitle>
-            </LogoContainer>
-              <JoinEventButton>Join Event</JoinEventButton>
-              <CreateEventButton>Create Event <Spotify src={SpotifyLogo} /></CreateEventButton>
+            <Logo isLogo={false} />
+            <JoinEventButton>Join Event</JoinEventButton>
+            <CreateEventButton>Create Event <Spotify src={SpotifyLogo} /></CreateEventButton>
           </HomePageContainer>
         </Background>
       </div>
