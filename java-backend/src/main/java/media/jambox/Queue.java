3cea212ab28f2bd8bad7a6a4bb989b838e50a9ab
Grace Brueggman <gbrueggman@Graces-MacBook-Pro.local>
2020-02-25 16:33:12 -0500

    queue sorting

--- java-backend/src/main/java/media/jambox/Queue.java
+++ java-backend/src/main/java/media/jambox/Queue.java
@@ -1,6 +1,7 @@
 package media.jambox;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Queue
 {
@@ -13,6 +14,7 @@ public class Queue
 
     public ArrayList<Track> sortAndDisplay()
     {
+        Collections.sort(trackList);
         return trackList;
     }
 
@@ -21,15 +23,33 @@ public class Queue
         return trackList.remove(0);
     }
 
+    /**
+     * add a track to the queue and reorder the queue.
+     * @param track track gained from user interaction
+     * @return updated queue
+     */
     public ArrayList<Track> append(Track track)
     {
         trackList.add(track);
+        trackList = sortAndDisplay();
         return trackList;
     }
 
-    public String remove(String trackId)
+    /**
+     * Removes a track from the queue via a string trackId.
+     * @param trackId String id to identify a track
+     * @return
+     */
+    public String removeTrack(String trackId)
     {
-        trackList.remove(trackId);
-        return trackId;
+        for (int i = 0; i < trackList.size() - 1; i++)
+        {
+            if (trackList.get(i).getId() == trackId)
+            {
+                trackList.remove(i);
+                return trackId;
+            }
+        }
+        return "[ERROR] could not find track to remove";
     }
 }
