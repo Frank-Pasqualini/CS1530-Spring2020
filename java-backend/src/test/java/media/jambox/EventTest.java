f36066dfe2cfaec8b025e59e5692544a455a8fd8
Frank Pasqualini <FJP11@pitt.edu>
2020-03-03 13:47:35 -0500

    refactored some test stuff

--- java-backend/src/test/java/media/jambox/EventTest.java
+++ java-backend/src/test/java/media/jambox/EventTest.java
@@ -1,30 +1,28 @@
 package media.jambox;
 
-import static org.junit.Assert.assertEquals;
-
 import com.wrapper.spotify.exceptions.SpotifyWebApiException;
 import java.io.IOException;
-import java.util.InputMismatchException;
-
 import org.junit.Before;
 import org.junit.Test;
 
-public class EventTest {
+public class EventTest
+{
     private final transient String accessToken = System.getenv("TEST_ACCESS_CODE");
     private final transient String accessTokenScoped = System.getenv("TEST_ACCESS_CODE_SCOPED");
     private final transient String playlistId = "45McaMvSG3vovfffyxEHz8";
     private final transient int eventCode = 1111;
     private final transient String hostId = "Host_01";
-    private transient Queue testQueue;
+    private transient Event testEvent;
 
     /**
      * Run before each test case.
+     *
      * @throws IOException e
      * @throws SpotifyWebApiException e
      */
     @Before
     public void setUp()
-            throws IOException, SpotifyWebApiException
+        throws IOException, SpotifyWebApiException
     {
         testEvent = new Event(eventCode, playlistId, accessToken, hostId);
     }
--- java-backend/src/test/java/media/jambox/QueueTest.java
+++ java-backend/src/test/java/media/jambox/QueueTest.java
@@ -14,8 +14,11 @@ public class QueueTest
 
     private final transient String accessToken = System.getenv("TEST_ACCESS_CODE");
     private final transient String accessTokenScoped = System.getenv("TEST_ACCESS_CODE_SCOPED");
-    private final transient String id = "45McaMvSG3vovfffyxEHz8";
+    private final transient String id = "65zguqi47ENgOSL285EIdU";
     private transient Queue testQueue;
+
+    private final transient String neverGonnaGiveYouUp = "7GhIk7Il098yCjg4BQjzvb";
+    private final transient String despacito = "6rPO02ozF3bM7NnOV4h6s2";
 
     /**
      * Run before each test case.
@@ -27,12 +30,12 @@ public class QueueTest
             throws IOException, SpotifyWebApiException
     {
         testQueue = new Queue(id, accessToken);
-        testQueue.vote("7GhIk7Il098yCjg4BQjzvb", 1);
-        testQueue.vote("7GhIk7Il098yCjg4BQjzvb", 1);
-        testQueue.vote("7GhIk7Il098yCjg4BQjzvb", -1);
-        testQueue.vote("7GhIk7Il098yCjg4BQjzvb", 1);
+        testQueue.vote(neverGonnaGiveYouUp, 1);
+        testQueue.vote(neverGonnaGiveYouUp, 1);
+        testQueue.vote(neverGonnaGiveYouUp, -1);
+        testQueue.vote(neverGonnaGiveYouUp, 1);
 
-        testQueue.vote("6rPO02ozF3bM7NnOV4h6s2", -1);
+        testQueue.vote(neverGonnaGiveYouUp, -1);
     }
 
     @Test
@@ -51,11 +54,11 @@ public class QueueTest
     public void testAppend()
     {
 
-        testQueue.append("6rPO02ozF3bM7NnOV4h6s2", accessToken); // track not yet in queue
+        testQueue.append(despacito, accessToken); // track not yet in queue
 
         try
         {
-            testQueue.append("6rPO02ozF3bM7NnOV4h6s2", accessToken); // track already in queue
+            testQueue.append(despacito, accessToken); // track already in queue
         }
         catch (InputMismatchException e)
         {
@@ -66,8 +69,8 @@ public class QueueTest
     @Test
     public void testRemoveTrackSucceed()
     {
-        testQueue.append("6rPO02ozF3bM7NnOV4h6s2", accessToken); // track not yet in queue
-        testQueue.removeTrack("6rPO02ozF3bM7NnOV4h6s2"); //remove track in queue
+        testQueue.append(despacito, accessToken); // track not yet in queue
+        testQueue.removeTrack(despacito); //remove track in queue
     }
 
     @Test
@@ -75,7 +78,7 @@ public class QueueTest
     {
         try
         {
-            testQueue.removeTrack("6rPO02ozF3bM7NnOV4h6s2");
+            testQueue.removeTrack(despacito);
         }
         catch (InputMismatchException e)
         {
@@ -87,24 +90,24 @@ public class QueueTest
     @Test
     public void testVoteUpSucceed()
     {
-        testQueue.append("6rPO02ozF3bM7NnOV4h6s2", accessToken); // track not yet in queue
-        testQueue.vote("6rPO02ozF3bM7NnOV4h6s2", 1); //vote +1 for track in queue
+        testQueue.append(despacito, accessToken); // track not yet in queue
+        testQueue.vote(despacito, 1); //vote +1 for track in queue
     }
 
     @Test
     public void testVoteDownSucceed()
     {
-        testQueue.append("6rPO02ozF3bM7NnOV4h6s2", accessToken); // track not yet in queue
-        testQueue.vote("6rPO02ozF3bM7NnOV4h6s2", -1); //vote -1 for track in queue
+        testQueue.append(despacito, accessToken); // track not yet in queue
+        testQueue.vote(despacito, -1); //vote -1 for track in queue
     }
 
     @Test
     public void testVoteAmountFailed()
     {
-        testQueue.append("6rPO02ozF3bM7NnOV4h6s2",  accessToken); // track not yet in queue
+        testQueue.append(despacito,  accessToken); // track not yet in queue
         try
         {
-            testQueue.vote("6rPO02ozF3bM7NnOV4h6s2", 2); //vote +2 for track in queue
+            testQueue.vote(despacito, 2); //vote +2 for track in queue
         }
         catch (InputMismatchException e)
         {
@@ -117,7 +120,7 @@ public class QueueTest
     {
         try
         {
-            testQueue.vote("6rPO02ozF3bM7NnOV4h6s2", 1); //vote +1 for track not in queue
+            testQueue.vote(despacito, 1); //vote +1 for track not in queue
         }
         catch (InputMismatchException e)
         {
